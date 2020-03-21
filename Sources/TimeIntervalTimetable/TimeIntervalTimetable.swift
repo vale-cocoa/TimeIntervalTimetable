16c73227f6fda94b0a7d1e52852909dee31bfc02
Valeriano Della Longa <valeriano.dellalonga@me.com>
2020-03-21 21:40:46 +0100

    Base struct and its test done

--- Sources/TimeIntervalTimetable/TimeIntervalTimetable.swift
+++ Sources/TimeIntervalTimetable/TimeIntervalTimetable.swift
@@ -5,6 +5,102 @@
 //  Created by Valeriano Della Longa on 21/03/2020.
 //  Copyright © 2020 Valeriano Della Longa. All rights reserved.
 //
+
+import Foundation
+
+/// A concrete `Schedule` type which represent a schedule timetable whose elements are occurring
+///  at a fixed `rate` interval, each having the same `duration` value.
+///
+/// Each element of the schedule timetable occours starting from `referenceDate`; that is given a
+/// `rate` value of `3600.0` and a `duration` value of `900.0`, there will be elements lasting
+///  15 minutes each and starting every hour before, on and after
+///   `Date(timeIntervalSinceReferenceDay: 0)`.
 struct TimeIntervalTimetable {
+    /// Errors thrown by this type.
+    public enum Error: Swift.Error {
+        /// The given `duration` parameter was wider than the given `rate` one.
+        case durationWiderThanRate
+        
+        /// Given date components are invalid for calculating the distance from reference date.
+        case invalidDateComponents
+    }
+    
+    /// The rate at which every element of the timetable occurs in time.
+    public let rate: TimeInterval
+    
+    /// The duration of each scheduled event of the timetable.
+    public let duration: TimeInterval
+    
+    /// Returns an instance of `TimeIntervalTimetable` with `rate` and `duration`
+    ///  values both set to `0.0`.
+    ///
+    /// That is by using `init()` an empty timetable schedule is returned.
+    public init() {
+        self.rate = 0.0
+        self.duration = 0.0
+    }
+    
+    /// Returns a`TimeIntervalTimetable`instance  initalized to given parameters.
+    ///
+    ///
+    /// - Parameter rate: The rate at which every elementof the timetable  occur.
+    /// Must be greater than or equal 0.
+    /// - Parameter duration: The duration of each schedule event of the timetable.
+    /// Must be greater than or equal 0.
+    /// - Returns: A `TimeIntervalTimetable`instance.
+    /// - Throws: `Error.durationWiderThanRate` when given `duration` param
+    ///  exceeds given `rate` one.
+    /// - Note: Both given parameters are rounded using
+    ///  `FloatingPointRoundingRule.towardZero` rounding rule. Thus precision of the
+    ///  timetable is to seconds.
+    public init(rate: TimeInterval, duration: TimeInterval) throws
+    {
+        let roundedRate = abs(rate.rounded(.towardZero))
+        let roundedDuration = abs(duration.rounded(.towardZero))
+        guard
+            roundedDuration <= roundedRate
+            else { throw Error.durationWiderThanRate }
+        
+        self.rate = roundedRate
+        self.duration = roundedDuration
+    }
+    
+    /// Returns valid `rate` and `duration` values from given `DateComponents` values,
+    /// each representing the distance between `referenceDate` and the value to obtain.
+    ///
+    /// That is each value will be calculated by using the `DateComponents` parameter to
+    /// calculate a corresponding `TimeInterval` value from
+    ///  `Date(TimeIntervalSinceReferenceDate: 0)` date.
+    /// - Parameter rateDC: The difference from reference date expressed in
+    /// `DateComponents` for calculating the a valid `rate` value.
+    /// - Parameter durationDC: The difference from reference date expressed in
+    /// `DateComponents` for calculating a valid `duration` value.
+    /// - Returns: a tuple containig both `rate` and `duration` valid for safely initialize a
+    /// `TimeIntervalTable` instance.
+    /// - Throws:`Error.invalidDateComponents` when it was impossible to extract a date
+    /// from one or both given parameters. `Error.durationWiderThanRate` when calculated
+    /// and rounded values are not valid in respect of `duration` less than or equal `rate`.
+    public static func validRateAndDurationFromDifferenceDateComponents(
+        rateDC: DateComponents,
+        durationDC: DateComponents
+    ) throws
+        -> (rate: TimeInterval, duration: TimeInterval)
+    {
+        let refDate = Date(timeIntervalSinceReferenceDate: 0)
+        guard
+            let rateDate = Calendar.current.date(byAdding: rateDC, to: refDate),
+            let durationDate = Calendar.current.date(byAdding: durationDC, to: refDate)
+            else { throw Error.invalidDateComponents }
+        
+        let rate = abs(refDate.distance(to: rateDate))
+            .rounded(.towardZero)
+        let duration = abs(refDate.distance(to: durationDate))
+            .rounded(.towardZero)
+        guard
+            duration <= rate
+            else { throw Error.durationWiderThanRate }
+        
+        return (rate, duration)
+    }
     
 }
--- Tests/TimeIntervalTimetableTests/TimeIntervalTimetableTests.swift
+++ Tests/TimeIntervalTimetableTests/TimeIntervalTimetableTests.swift
@@ -9,15 +9,122 @@ import XCTest
 @testable import TimeIntervalTimetable
 
 final class TimeIntervalTimetableTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
+    
+    func test_init_returnsEmpty() {
+        // given
+        // when
+        let timetable = TimeIntervalTimetable()
         
+        // then
+        XCTAssertEqual(timetable.rate, 0.0)
+        XCTAssertEqual(timetable.duration, 0.0)
+    }
+    
+    func test_init_whenDurationIsGreaterThanRate_throws() {
+        // given
+        let rate = TimeInterval.random(in: 1.0...60.0)
+        let duration = rate + 1.0
+        
+        // when
+        // then
+        XCTAssertThrowsError(try TimeIntervalTimetable(rate: rate, duration: duration), "rate: \(rate) - duration: \(duration)")
     }
 
+    func test_init_whenDurationIsLessThanOrEqualRate_doesntThrow()
+    {
+        // given
+        let rate = TimeInterval.random(in: 1.0...60.0)
+        let durationLessThanRate = rate - 1.0
+        let durationEqualRate = rate
+        
+        // when
+        // then
+        XCTAssertNoThrow(try TimeIntervalTimetable(rate: rate, duration: durationEqualRate))
+        XCTAssertNoThrow(try TimeIntervalTimetable(rate: rate, duration: durationLessThanRate))
+    }
+    
+    func test_init_whenDoesntThrow_setsDurationandRateToRoundedValues()
+    {
+        // given
+        let rate = TimeInterval.random(in: 1.0...60.0)
+        let duration = rate - 1.0
+        let expectedRate = rate.rounded(.towardZero)
+        let expectedDuration = duration.rounded(.towardZero)
+        
+        // when
+        let timetable = try! TimeIntervalTimetable(rate: rate, duration: duration)
+        
+        // then
+        XCTAssertEqual(timetable.rate, expectedRate)
+        XCTAssertEqual(timetable.duration, expectedDuration)
+    }
+    
+    func test_validRateAndDurationFrom_whenRateIsSmallerThanDuration_throwsExpectedError()
+    {
+        // given
+        let rateDC = DateComponents(hour: 1)
+        let durationDC = DateComponents(day: 1)
+        let expectedResult = TimeIntervalTimetable.Error.durationWiderThanRate as NSError
+        
+        do {
+            // when
+            _ = try TimeIntervalTimetable.validRateAndDurationFromDifferenceDateComponents(rateDC: rateDC, durationDC: durationDC)
+            
+            // then
+            XCTFail("Didn't throw error: rateDC: \(rateDC), durationDC: \(durationDC)")
+        } catch {
+            // when
+            let resultError = error as NSError
+            
+            // then
+            XCTAssertEqual(resultError.domain, expectedResult.domain)
+            XCTAssertEqual(resultError.code, expectedResult.code)
+        }
+        
+    }
+    
+    func test_validRateAndDurationFrom_whenRateIsGreaterThanOrEqualDuration_doesntThrow()
+    {
+        // given
+        // when
+        let rateDC = DateComponents(hour: 1)
+        let equalDurationDC = DateComponents(hour: 1)
+        let smallerDurationDC = DateComponents(minute: 15)
+        
+        // then
+        XCTAssertNoThrow(try TimeIntervalTimetable.validRateAndDurationFromDifferenceDateComponents(rateDC: rateDC, durationDC: equalDurationDC))
+        XCTAssertNoThrow(try TimeIntervalTimetable.validRateAndDurationFromDifferenceDateComponents(rateDC: rateDC, durationDC: smallerDurationDC))
+    }
+    
+    func test_validRateAndDurationFrom_whenRateIsGreaterThanOrEqualDuration_returnsEcpectedValues()
+    {
+        // given
+        let expectedResultForEqual: (TimeInterval, TimeInterval) = (3600.0, 3600.0)
+        let expectedResultForSmaller: (TimeInterval, TimeInterval) = (3600.0, 900.0)
+        
+        // when
+        let rateDC = DateComponents(hour: 1)
+        let equalDurationDC = DateComponents(hour: 1)
+        let smallerDurationDC = DateComponents(minute: 15)
+        
+        let resultForEqual = try! TimeIntervalTimetable.validRateAndDurationFromDifferenceDateComponents(rateDC: rateDC, durationDC: equalDurationDC)
+        let resultForSmaller = try! TimeIntervalTimetable.validRateAndDurationFromDifferenceDateComponents(rateDC: rateDC, durationDC: smallerDurationDC)
+        
+        // then
+        XCTAssertEqual(resultForEqual.0, expectedResultForEqual.0)
+        XCTAssertEqual(resultForEqual.1, expectedResultForEqual.1)
+        XCTAssertEqual(resultForSmaller.0, expectedResultForSmaller.0)
+        XCTAssertEqual(resultForSmaller.1, expectedResultForSmaller.1)
+    }
+    
     static var allTests = [
-        ("testExample", testExample),
+        ("test_init_returnsEmpty", test_init_returnsEmpty),
+        ("test_init_whenDurationIsGreaterThanRate_throws", test_init_whenDurationIsGreaterThanRate_throws),
+        ("test_init_whenDurationIsLessThanOrEqualRate_doesntThrow", test_init_whenDurationIsLessThanOrEqualRate_doesntThrow),
+        ("test_init_whenDoesntThrow_setsDurationandRateToRoundedValues", test_init_whenDoesntThrow_setsDurationandRateToRoundedValues),
+        ("test_validRateAndDurationFrom_whenRateIsSmallerThanDuration_throwsExpectedError", test_validRateAndDurationFrom_whenRateIsSmallerThanDuration_throwsExpectedError),
+        ("test_validRateAndDurationFrom_whenRateIsGreaterThanOrEqualDuration_doesntThrow", test_validRateAndDurationFrom_whenRateIsGreaterThanOrEqualDuration_doesntThrow),
+        ("test_validRateAndDurationFrom_whenRateIsGreaterThanOrEqualDuration_returnsEcpectedValues", test_validRateAndDurationFrom_whenRateIsGreaterThanOrEqualDuration_returnsEcpectedValues),
         
     ]
 }
